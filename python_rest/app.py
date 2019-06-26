65b136b2ee26771ca0fd7f673c76b137c9b35641
Xa1n <xhs893@gmail.com>
2019-06-26 13:54:17 +0100

    Add post method to add tasks to database

--- python_rest/app.py
+++ python_rest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, make_response
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
@@ -32,6 +32,23 @@ def get_task(task_id):
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+@app.route('/todo/api/tasks', methods=['POST'])         # insert a new item into task database
+def create_task():
+    if not request.json or not 'title' in request.json:         # ensures error if data isn't there or doesn't have 'title' item
+        abort(400)
+    task = {                                            # 
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
